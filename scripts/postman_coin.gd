ba2f4062349183b2ba3428f845fde1f03dd8a40e
Rey <reydltorre@gmail.com>
2020-12-05 21:01:51 -0800

    coins hud and signals logic

--- scripts/postman_coin.gd
+++ scripts/postman_coin.gd
@@ -5,6 +5,7 @@ signal postman_coin_collected
 func _on_postman_coin_body_entered(body):
 	$AnimationPlayer.play("bounce")
 	emit_signal("postman_coin_collected")
+	set_collision_mask_bit(0, false)
 	
 
 func _on_AnimationPlayer_animation_finished(anim_name):
