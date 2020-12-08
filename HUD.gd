dddb2bca51702ff5d0a0a0ef4be8f5a68359b2d4
Rey <reydltorre@gmail.com>
2020-12-08 13:52:15 -0800

    removing troll coins, kinda dumb

--- HUD.gd
+++ HUD.gd
@@ -3,18 +3,15 @@ extends CanvasLayer
 var insomnia_coins = 0
 var postman_coins = 0
 var techlead_coins = 0
-var troll_coins = 0
 
 const INSOMNIA_COINS_MAX_WIN = 8
 const POSTMAN_COINS_MAX_WIN = 10
 const TECHLEAD_COINS_MAX_WIN = 2
-const TROLL_COINS_MAX_WIN = 1
 
 
 func _ready():
 	$insomnia_label.text = String(insomnia_coins)
 	$postman_label.text = String(postman_coins)
-	$troll_label.text = String(troll_coins)
 
 func _physics_process(delta):
 	# check if all maxes are met
@@ -29,7 +26,3 @@ func _on_postman_coin_collected():
 	postman_coins = postman_coins + 1
 	_ready()
 
-
-func _on_troll_coin_collected():
-	troll_coins = troll_coins + 1
-	_ready()
--- Hagay.gd
+++ Hagay.gd
@@ -2,6 +2,7 @@ extends KinematicBody2D
 
 var velocity = Vector2(0, 0)
 export var direction = 1
+export var detect_edges = true
 
 const GRAVITY = 20
 const SPEED = 100
@@ -9,16 +10,22 @@ const SPEED = 100
 func _ready():
 	$AnimatedSprite.flip_h = (false if direction == 1 else true)
 	$RayCast2D.position.x = $CollisionShape2D.shape.get_extents().x * direction
-
+	$RayCast2D.enabled = detect_edges
 	
-func _physics_process(delta):
+
+func apply_gravity():
 	if not is_on_floor():
 		velocity.y += GRAVITY
-		
-	if is_on_wall() or not $RayCast2D.is_colliding():
+	
+func wall_collide():
+	if is_on_wall() or not $RayCast2D.is_colliding() and detect_edges and is_on_floor():
 		direction = direction * -1
 		$AnimatedSprite.flip_h = not $AnimatedSprite.flip_h
 		$RayCast2D.position.x = $CollisionShape2D.shape.get_extents().x * direction
+	
+func _physics_process(delta):
+	apply_gravity()
+	wall_collide()
 		
 	velocity.x = SPEED * direction
 	
